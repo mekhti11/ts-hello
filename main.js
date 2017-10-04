90f38ced33414d3781cebda620230b1306cd3162
mekhti11 <mekhti11@gmail.com>
2017-10-04 16:50:18 +0300

    Types Assertions and arrow functions

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+/*Prints message on screen*/
 function yaz(message) {
     console.log(message);
 }
-var message = "Hello world!";
+/*An Arrow function prints message on screen*/
+var doprint = function (param) {
+    console.log(param);
+};
+var num1;
+var message;
+message = 'Hello world!';
+var charAt = message.charAt(2);
+var charAtAlternative = message.charAt(3);
 yaz(message);
+yaz(charAt);
+yaz(charAtAlternative);
+doprint(message);
--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /*Prints message on screen*/
-function yaz(message) {
+function yaz(message:any) {
   console.log(message);
 }
+/*An Arrow function prints message on screen*/
+let doprint = (param:any) => {
+  console.log(param);
+}
 
-let message = "Hello world!";
+
+let num1 : boolean;
+
+let message;
+message = 'Hello world!';
+
+let charAt = (<string> message).charAt(2) ;
+yaz(charAt);
+
+let charAtAlternative = (message as string).charAt(3);
+yaz(charAtAlternative);
 
 yaz(message);
+doprint(message);
