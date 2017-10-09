ca5b0c49ce2a5edb19b4c5919dbba4156e3409c0
mekhti11 <mekhti11@gmail.com>
2017-10-10 01:24:18 +0300

    classes + objects + constructors

--- main.js
+++ main.js
@@ -6,14 +6,36 @@ function yaz(message) {
 var doprint = function (point) {
     console.log(point.isim + point.mesaj);
 };
-var message;
-message = 'Hello world!';
-var charAt = message.charAt(2);
-yaz(charAt);
-var charAtAlternative = message.charAt(3);
-yaz(charAtAlternative);
-yaz(message);
+var Man = /** @class */ (function () {
+    function Man(isim, yas) {
+        this.isim = isim;
+        this.yas = yas;
+    }
+    Man.prototype.yaz = function () {
+        console.log("Name :" + this.isim + "\nAge :" + this.yas);
+    };
+    Man.prototype.getOlder = function (another) {
+        if (this.yas > another.yas)
+            return this;
+        return another;
+    };
+    return Man;
+}());
 doprint({
     mesaj: 'says Hello',
     isim: 'mekhti '
 });
+var yavsax = new Man('Yaver', 20);
+yavsax.yaz();
+/*
+let message;
+message = 'Hello world!';
+
+let charAt = (<string> message).charAt(2) ;
+yaz(charAt);
+
+let charAtAlternative = (message as string).charAt(3);
+yaz(charAtAlternative);
+
+yaz(message)
+*/
--- main.ts
+++ main.ts
@@ -13,8 +13,35 @@ let doprint = (point:Point) => {
   console.log(point.isim + point.mesaj);
 }
 
+class Man{
+  isim : string;
+  yas : number;
 
+  constructor(isim:string , yas:number){
+    this.isim = isim;
+    this.yas = yas;
+  }
 
+  yaz(){
+    console.log("Name :"+this.isim+"\nAge :"+this.yas);
+  }
+
+  getOlder(another:Man){
+    if(this.yas> another.yas)
+      return this;
+    return another;
+  }
+
+}
+
+doprint({
+  mesaj:'says Hello',
+  isim : 'mekhti '
+})
+
+let yavsax : Man = new Man('Yaver',20);
+yavsax.yaz();
+/*
 let message;
 message = 'Hello world!';
 
@@ -25,8 +52,4 @@ let charAtAlternative = (message as string).charAt(3);
 yaz(charAtAlternative);
 
 yaz(message)
-
-doprint({
-  mesaj:'says Hello',
-  isim : 'mekhti '
-})
+*/
