a36216c74f7982ea026ea92168333a73c3c50347
mekhti11 <mekhti11@gmail.com>
2017-10-09 23:06:23 +0300

    interfaces

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ function yaz(message) {
     console.log(message);
 }
 /*An Arrow function prints message on screen*/
-var doprint = function (param) {
-    console.log(param);
+var doprint = function (point) {
+    console.log(point.isim + point.mesaj);
 };
-var num1;
 var message;
 message = 'Hello world!';
 var charAt = message.charAt(2);
-var charAtAlternative = message.charAt(3);
-yaz(message);
 yaz(charAt);
+var charAtAlternative = message.charAt(3);
 yaz(charAtAlternative);
-doprint(message);
+yaz(message);
+doprint({
+    mesaj: 'says Hello',
+    isim: 'mekhti '
+});
--- main.ts
+++ main.ts
@@ -2,13 +2,18 @@
 function yaz(message:any) {
   console.log(message);
 }
+
+interface Point{
+  mesaj : string
+  isim : string,
+}
+
 /*An Arrow function prints message on screen*/
-let doprint = (param:any) => {
-  console.log(param);
+let doprint = (point:Point) => {
+  console.log(point.isim + point.mesaj);
 }
 
 
-let num1 : boolean;
 
 let message;
 message = 'Hello world!';
@@ -19,5 +24,9 @@ yaz(charAt);
 let charAtAlternative = (message as string).charAt(3);
 yaz(charAtAlternative);
 
-yaz(message);
-doprint(message);
+yaz(message)
+
+doprint({
+  mesaj:'says Hello',
+  isim : 'mekhti '
+})
